4e67b9ecb39481214d19d362098cb22c58f32e44
EleonoraGalieva <48092813+EleonoraGalieva@users.noreply.github.com>
2020-05-23 02:06:40 +0300

    Lab 1, 2

---
+++ task1.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import plotly.graph_objects as go
+
+
+# Var 3
+# Y=|x|; a=-3; b=3;
+
+# СВ Yi
+def make_y_i(n, a, b):
+    sigma_i = np.random.sample(n)
+    x_i = list(map(lambda x: x * (b - a) + a, sigma_i))
+    y_i = list(map(lambda y: math.fabs(y), x_i))
+    y_i.sort()
+    return y_i
+
+
+if __name__ == '__main__':
+    # Lab1
+    a = -3
+    b = 3
+    n = int(input())
+    # Формирование выборки СВ
+    y_i = make_y_i(n, a, b)
+    # Тк вероятность того, что в выборке встретится два одинаковых числа -> 0, будем считать,
+    # что частота появления каждой величины = 1
+    n_i = [1] * n
+    # Посчитаем кол-во наборов, при которых случайное значение Y<y
+    n_x = []
+    temp = 1
+    for i in range(1, n + 1):
+        n_x.append(temp)
+        temp += 1
+    # Найдем эмпирическую функцию распределения
+    F_practical = list(map(lambda x: x / n, n_x))
+    # И теоритическую, через формулы нахождения функции СВ (посчитала для конкретной функции вручную)
+    F_theoretical = list(map(lambda y: 1 / 3 * y, y_i))
+    # Строим графики и таблицы
+    figure = plt.figure()
+    subplot = figure.add_subplot(111)
+    subplot.plot(y_i, F_theoretical)
+    subplot.step(y_i, F_practical, "g-o", where="pre")
+    fig = go.Figure(
+        data=[
+            go.Table(header=dict(values=['Эмпирическая функция распределения', 'Теоретическая функция распределения']),
+                     cells=dict(values=[F_practical, F_theoretical]))
+        ])
+    fig.write_html('tmp.html', auto_open=True)
+    plt.show()
---
+++ task2.py
@@ -0,0 +1,129 @@
+import math
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from task1 import make_y_i
+
+
+def make_n_x(n_i):
+    n_x = []
+    temp = n_i[0]
+    for i in range(1, len(n_i)):
+        n_x.append(temp)
+        temp += n_i[i]
+    n_x.append(temp)
+    return n_x
+
+
+# Функция для определения кол-ва интервалов, на которые нужно разбить выборку
+def find_amount_of_intervals(n):
+    if n <= 100:
+        amount_of_intervals = int(math.sqrt(n))
+    else:
+        amount_of_intervals = int(3 * math.log(n, 2))
+    return amount_of_intervals
+
+
+# Используется для равновероятностного метода.
+# Если размер выборки нацело не делится на кол-во интервалов, то уменьшаем кол-во интервалов
+def check_amount_of_intervals(n, amount_of_intervals):
+    while n % amount_of_intervals != 0:
+        amount_of_intervals -= 1
+    return amount_of_intervals
+
+
+# Графики и таблицы
+def visualisation(A_i, f_i):
+    xlist = []
+    ylist = []
+    for i in range(0, amount_of_intervals):
+        xlist.append(A_i[i])
+        xlist.append(B_i[i])
+        ylist.append(f_i[i])
+        ylist.append(f_i[i])
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(
+        nrows=1, ncols=4,
+        figsize=(8, 4)
+    )
+    ax1.plot(xlist, ylist)
+    ax1.set_title("Гистограмма")
+    ax2.plot(A_i, n_i_lr2)
+    ax2.set_title("Полигон частот")
+    n_x = make_n_x(n_i_lr2)
+    F_practical = list(map(lambda x: x / n, n_x))
+    ax3.step(n_x, F_practical, "g-o", where="pre")
+    ax3.set_title("Эмпирическая F")
+    ylist = []
+    for i in range(0, len(xlist)):
+        ylist.append(1 / 3)  # плотность для конкретной функции
+    ax4.plot(xlist, ylist)
+    ax4.set_title("Плотность распределения")
+    table1 = go.Figure(
+        data=[
+            go.Table(header=dict(values=['Ai', 'Bi', 'Количество СВ на данном интервале', 'Значения F эмпирической']),
+                     cells=dict(values=[A_i, B_i, n_i_lr2, F_practical]))
+        ])
+    table1.write_html('tmp.html', auto_open=True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Lab2
+    n = int(input())
+    y_i = make_y_i(n, -3, 3)
+    # Равноинтервальный
+    amount_of_intervals = find_amount_of_intervals(n)
+    # Получаем длину интервала
+    len_of_interval = (y_i[-1] - y_i[0]) / amount_of_intervals
+    # Находим границы каждого из интервалов
+    A_i = []
+    B_i = []
+    i = 0
+    temp = y_i[0]
+    while i != amount_of_intervals:
+        A_i.append(temp)
+        temp = A_i[-1] + len_of_interval
+        i += 1
+    for i in range(1, len(A_i)):
+        B_i.append(A_i[i])
+    B_i.append(A_i[-1] + len_of_interval)
+    # Находим кол-во СВ, которые попали в данный интервал
+    n_i_lr2 = []
+    amount_of_nums_in_interval = 0
+    for i in range(0, len(A_i)):
+        for j in range(0, len(y_i)):
+            if B_i[i] >= y_i[j] >= A_i[i]:
+                amount_of_nums_in_interval += 1
+        n_i_lr2.append(amount_of_nums_in_interval)
+        amount_of_nums_in_interval = 0
+    # Вычисляем среднюю плотность вероятности для каждого интервала
+    f_i = []
+    for i in range(0, amount_of_intervals):
+        f_i.append(n_i_lr2[i] / (n * len_of_interval))
+    visualisation(A_i, f_i)
+
+    # Равновероятностный
+    amount_of_intervals = find_amount_of_intervals(n)
+    # Вычисляем оптимальное кол-во СВ
+    amount_of_nums_in_interval = check_amount_of_intervals(n, amount_of_intervals)
+    n_i_lr2 = []
+    for i in range(0, amount_of_intervals):
+        n_i_lr2.append(amount_of_nums_in_interval)
+    # Находим границы каждого из интервалов
+    A_i = []
+    B_i = []
+    i = 0
+    while i != n:
+        A_i.append(y_i[i])
+        i += amount_of_nums_in_interval
+    for i in range(1, len(A_i)):
+        B_i.append(A_i[i])
+    B_i.append(y_i[-1])
+    # Вычисляем длину каждого из интервалов
+    h_i = []
+    for i in range(0, len(A_i)):
+        h_i.append(B_i[i] - A_i[i])
+    # Вычисляем среднюю плотность вероятности для каждого интервала
+    f_i = []
+    for i in range(0, amount_of_intervals):
+        f_i.append(n_i_lr2[i] / (n * h_i[i]))
+    visualisation(A_i, f_i)
