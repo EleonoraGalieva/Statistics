261dc912a0456f303fb04e4296d3c2d04cd1a879
EleonoraGalieva <48092813+EleonoraGalieva@users.noreply.github.com>
2020-05-25 22:39:46 +0300

    Update task1.py
    Добавила вариационный ряд в таблицу, достроила график эмпирической функции распределения. Из-за того, что в вариационном ряде может не встретиться значений 0 и 3, в которых теоретическая функция распределения принимает значения 0 и 1 соответственно (особенно это касается 1), то при малых n значение теоретической функции может мало того, что значительно отличаться от эмпирической, но и не иметь близкое к 1 значение для последнего члена вариационного ряда.

--- task1.py
+++ task1.py
@@ -9,11 +9,19 @@ import plotly.graph_objects as go
 
 # СВ Yi
 def make_y_i(n, a, b):
-    sigma_i = np.random.sample(n)
+    sigma_i = np.random.rand(n)
     x_i = list(map(lambda x: x * (b - a) + a, sigma_i))
     y_i = list(map(lambda y: math.fabs(y), x_i))
     y_i.sort()
     return y_i
+
+
+def M(a, b):
+    return (a + b) / 2
+
+
+def D(a, b):
+    return (b - a) ** 2
 
 
 if __name__ == '__main__':
@@ -28,10 +36,10 @@ if __name__ == '__main__':
     n_i = [1] * n
     # Посчитаем кол-во наборов, при которых случайное значение Y<y
     n_x = []
-    temp = 1
-    for i in range(1, n + 1):
+    temp = 0
+    for i in range(0, n):
+        temp += n_i[i]
         n_x.append(temp)
-        temp += 1
     # Найдем эмпирическую функцию распределения
     F_practical = list(map(lambda x: x / n, n_x))
     # И теоритическую, через формулы нахождения функции СВ (посчитала для конкретной функции вручную)
@@ -39,12 +47,16 @@ if __name__ == '__main__':
     # Строим графики и таблицы
     figure = plt.figure()
     subplot = figure.add_subplot(111)
+    subplot.plot([y_i[0] - 1, y_i[0]], [0, 0], color="green")
+    subplot.scatter(y_i[0], 0, color="green")
     subplot.plot(y_i, F_theoretical)
-    subplot.step(y_i, F_practical, "g-o", where="pre")
+    subplot.step(y_i, F_practical, "g-o", where="post")
+    subplot.plot([y_i[-1], y_i[-1] + 1], [1, 1], color="green")
     fig = go.Figure(
         data=[
-            go.Table(header=dict(values=['Эмпирическая функция распределения', 'Теоретическая функция распределения']),
-                     cells=dict(values=[F_practical, F_theoretical]))
+            go.Table(header=dict(values=['Вариационный ряд', 'Эмпирическая функция распределения',
+                                         'Теоретическая функция распределения']),
+                     cells=dict(values=[y_i, F_practical, F_theoretical]))
         ])
     fig.write_html('tmp.html', auto_open=True)
     plt.show()
